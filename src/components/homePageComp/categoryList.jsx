ce3cb87ff82bb1e9e24ad05bedf78f6a274449a2
SiteOcean <tosathishsite@gmail.com>
2024-03-01 22:50:50 +0530

    new update

--- src/components/homePageComp/categoryList.jsx
+++ src/components/homePageComp/categoryList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CategoryList = ({ cat, SelectBrand }) => {
+const CategoryList = ({ cat, SelectBrand , catName}) => {
 
   return (
-    <li onClick={() => SelectBrand(cat)} className='list-none block w-[500px]'>
+    <li onClick={() => SelectBrand(cat)} className={`list-none block w-[500px] ${catName === cat && "bg-red-300"}`}>
     {cat.length > 10 ? cat.slice(0, 10) + '...' : cat}
   </li>
   
--- src/components/homePageComp/mainComp.jsx
+++ src/components/homePageComp/mainComp.jsx
@@ -4,35 +4,44 @@ import CategoryList from './categoryList';
 
 import ProductCard from './productsCard';
 let products = null;
+let catName = 'all';
 export default function MainHomeComp (){
 
     const categories = ['Nokia', 'Samsung', 'Apple', 'Sony'];
     const [pro, setPro]= useState(null)
     
+  
+
+    const fetchData = async () => {
+        try {
+          const response = await fetch('https://dummyjson.com/products');
+          const json = await response.json();
+          setPro(json.products);
+          products = json.products;
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+    useEffect(()=>{
+       
+        fetchData();
+    },[]);
+
     const SelectBrand=(cat)=>{
+        catName = cat;
+        if(cat === "all"){
+            fetchData() 
+        }
             setPro(
                 products.filter((val,i)=>{
                     return cat === val.brand
                 })
             )
+
     }
 
     useEffect(()=>{
-        const fetchData = async () => {
-            try {
-              const response = await fetch('https://dummyjson.com/products');
-              const json = await response.json();
-              setPro(json.products);
-              products = json.products;
-            } catch (error) {
-              console.error('Error fetching data:', error);
-            }
-          };
-        fetchData();
-    },[]);
-
-    useEffect(()=>{
-        console.log("test")
         setPro(products)
     },[products])
 
@@ -42,11 +51,14 @@ export default function MainHomeComp (){
 
             <div className='flex gap-x-3 overflow-auto w-full justify-center p-2 items-center'>
             <ul className='flex gap-x-6 w-[82%] mx-auto overflow-auto'>
+            <li onClick={() => SelectBrand("all")} className={`list-none block w-[500px] ${catName === "all" && "bg-red-300"}`}>
+                        All
+                    </li>
                 {products && products.length > 0 && (
                     products
                     .filter((cat, index, self) => self.findIndex((c) => c.brand === cat.brand) === index)
                     .map((uniqueCat, i) => (
-                        <CategoryList key={i} cat={uniqueCat.brand} SelectBrand={SelectBrand}/>
+                        <CategoryList key={i} catName={catName} cat={uniqueCat.brand} SelectBrand={SelectBrand}/>
                     ))
                 )
                 }
